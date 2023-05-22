38e36fc7ba7467a68e74dfbf739a4496a6865700
bl96 <47901930+bl96@users.noreply.github.com>
2023-05-22 17:36:06 +0200

    Improved DateTime insert format and description

--- src/main/java/org/hypergraphql/schemaextraction/ExtendedScalars.java
+++ src/main/java/org/hypergraphql/schemaextraction/ExtendedScalars.java
@@ -4,13 +4,19 @@ import graphql.language.IntValue;
 import graphql.language.StringValue;
 import graphql.schema.*;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.time.DateTimeException;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 public class ExtendedScalars {
+
+    private static final String DATETIME_PATTERN = "yyyy-MM-dd'T'HH:mmZZ";
 
     private static String typeName(Object input) {
         if (input == null) {
@@ -30,7 +36,7 @@ public class ExtendedScalars {
     /**
      * This represents the "DateTime" type which is a representation of org.joda.time.DateTime
      */
-    public static final GraphQLScalarType GraphQLDateTime = GraphQLScalarType.newScalar().name("DateTime").description("Built-in simplified org.joda.time.DateTime. Returns millis and string representation in ISO format.").coercing(new Coercing<DateTime, DateTime>() {
+    public static final GraphQLScalarType GraphQLDateTime = GraphQLScalarType.newScalar().name("DateTime").description("Built-in simplified org.joda.time.DateTime. Returns millis and string representation in ISO format. The format should be 2010-06-30T01:20+02:00 or DateTime in milliseconds.").coercing(new Coercing<DateTime, DateTime>() {
 
         private DateTime convertImpl(Object input) {
             if (isDate(input)) {
@@ -66,11 +72,40 @@ public class ExtendedScalars {
             return result;
         }
 
+        private boolean isValidDate(String dateTimeString) {
+            boolean valid = true;
+            try {
+                DateTimeFormatter formatter = DateTimeFormat.forPattern(DATETIME_PATTERN);
+                DateTime dob = formatter.parseDateTime(dateTimeString);
+            } catch (Exception e) {
+                valid = false;
+            }
+            return valid;
+        }
+
+        public static boolean isNumeric(String dateTimeString) {
+            try {
+                Double.parseDouble(dateTimeString);
+                return true;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+
         @Override
         public DateTime parseLiteral(Object input) {
             if (input instanceof StringValue) {
                 try {
-                    return new DateTime(((StringValue) input).getValue());
+                    String dateTimeString = ((StringValue) input).getValue();
+
+                    boolean isNumericDate = isNumeric(dateTimeString);
+
+                    if (!isValidDate(dateTimeString) && !isNumericDate) {
+                        throw new DateTimeException(
+                                "Wrong DateTime format for input:" + input + ". Please use the format 2010-06-30T01:20+02:00 or DateTime in milliseconds."
+                        );
+                    }
+                    return isNumericDate ? new DateTime((Long.parseLong(dateTimeString) * 1000L), DateTimeZone.UTC) : new DateTime(dateTimeString);
                 } catch (NumberFormatException e) {
                     throw new DateTimeException(
                             "Unable to turn AST input into a 'DateTime' : '" + input + "'"
